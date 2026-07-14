06004dd819d295fb3e41c870a03ab3bb08008b91
chiuhsiaohsuan <qwe633522@gmail.com>
2026-07-14 18:10:57 +0800

    07/14

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Home } from './home/home';
 import { About } from './about/about';
 import{ News } from './news/news';
 import{ Product } from './product/product';
+import{ Login } from './login/login';
+import{ Register } from './register/register';
 
 export const routes: Routes = [
     {path: '', component: Home},
     {path: 'about', component: About},
     {path: 'news', component: News},
     {path: 'product', component: Product},
+    {path: 'login', component: Login},
+    {path: 'register', component: Register},
 ];
--- src/app/product/product.ts
+++ src/app/product/product.ts
@@ -1,42 +1,169 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+
 
 @Component({
   selector: 'app-product',
-  imports: [],
   templateUrl: './product.html',
-  styleUrl: './product.css',
+  styleUrl: './product.css'
 })
+
 export class Product {
-    products = [
+
+
+  products = [
+
     {
-      name: '保濕精華液',
-      price: 980,
-      image: 'example.jpg'
+      name:'保濕精華液',
+      price:980,
+      image:'example.jpg',
+      category:'保養品'
+    },
+
+    {
+      name:'修護乳液',
+      price:1280,
+      image:'pic.jpg',
+      category:'保養品'
+    },
+
+    {
+      name:'亮白化妝水',
+      price:790,
+      image:'pic.jpg',
+      category:'化妝水'
+    },
+
+    {
+      name:'玻尿酸面膜',
+      price:680,
+      image:'example.jpg',
+      category:'面膜'
+    },
+
+    {
+      name:'卸妝油',
+      price:520,
+      image:'pic.jpg',
+      category:'清潔用品'
+    },
+
+    {
+      name:'防曬乳',
+      price:880,
+      image:'example.jpg',
+      category:'防曬'
     },
     {
-      name: '修護乳液',
-      price: 1280,
-      image: 'pic.jpg'
+      name:'玻尿酸面膜',
+      price:680,
+      image:'example.jpg',
+      category:'面膜'
     },
+
     {
-      name: '亮白化妝水',
-      price: 790,
-      image: 'pic.jpg'
+      name:'卸妝油',
+      price:520,
+      image:'pic.jpg',
+      category:'清潔用品'
     },
+
     {
-      name: '玻尿酸面膜',
-      price: 680,
-      image: 'example.jpg'
-    },
-    {
-      name: '卸妝油',
-      price: 520,
-      image: 'pic.jpg'
-    },
-    {
-      name: '防曬乳',
-      price: 880,
-      image: 'example.jpg'
+      name:'防曬乳',
+      price:880,
+      image:'example.jpg',
+      category:'防曬'
     }
+
   ];
+
+  categories = [
+    '全部',
+    '保養品',
+    '化妝水',
+    '面膜',
+    '清潔用品',
+    '防曬'
+  ];
+
+  selectedCategory = signal('全部');
+
+  filterProducts = computed(()=>{
+
+
+    const category = this.selectedCategory();
+
+
+    if(category === '全部'){
+      return this.products;
+    }
+
+
+    return this.products.filter(
+      item => item.category === category
+    );
+
+  });
+
+
+// 每頁數量
+  pageSize = 6;
+
+
+  //目前頁
+  currentPage = signal(1);
+
+
+  //總頁數
+  pages = computed(()=>{
+
+
+    const total = Math.ceil(
+      this.filterProducts().length / this.pageSize
+    );
+
+
+    return Array.from(
+      {length:total},
+      (_,i)=>i+1
+    );
+
+  });
+
+
+
+  //目前頁商品
+  pagedProducts = computed(()=>{
+
+
+    const start =
+      (this.currentPage()-1) * this.pageSize;
+
+
+    return this.filterProducts()
+      .slice(
+        start,
+        start + this.pageSize
+      );
+
+  });
+
+
+
+  //切換頁面
+  changePage(page:number){
+
+    this.currentPage.set(page);
+
+  }
+
+
+
+  //切換分類時回第一頁
+  changeCategory(category:string){
+
+    this.selectedCategory.set(category);
+
+    this.currentPage.set(1);
+
+  }
 }
